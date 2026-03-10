96f0f2a0fd8bc91e3390dfd97ac8659b8a830f7f
Gbenge Aondoakula Raphael <gbengeraphael@gmail.com>
2026-03-10 21:00:24 +0100

    feat: Initialize iOS and macOS Podfiles, configure Android release build settings, update app version, and add screenshots.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,10 @@
+import java.util.Properties
+import java.io.FileInputStream
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -25,16 +32,34 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
-
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it as String) }
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+        getByName("release") {
+            // Enable signing (as configured previously)
+            signingConfig = signingConfigs.getByName("release")
+
+            // Enable code shrinking and obfuscation
+            isMinifyEnabled = true
+
+            // Enable resource shrinking (removes unused assets/xml)
+            isShrinkResources = true
+
+            // Use the default ProGuard rules included with the Android SDK
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
